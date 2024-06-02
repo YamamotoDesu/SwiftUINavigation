721dddb3a80838f15017cb71c24594d7761853ba
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2024-06-02 14:58:21 +0900

    Initial Commit

---
+++ SwiftUINavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUINavigation
+//
+//  Created by Yamamoto Kyo on 2024/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Yamamoto Kyo on 2024/06/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
